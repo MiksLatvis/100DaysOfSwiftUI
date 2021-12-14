cf8c128415aead13e8c8b27d30ece6e32393f1a6
Miks Latvis <miks.latvis@gmail.com>
2021-12-14 22:04:12 +0900

    End of day 13

--- 100DaysOfSwiftUI.playground/Contents.swift
+++ 100DaysOfSwiftUI.playground/Contents.swift
@@ -1,8 +1,14 @@
-import SwiftUI
+// Day 1
+
+// Day 9
+// closures, passing functions into functions, and checkpoint 5
 
 let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
 print(luckyNumbers.sorted().filter{!$0.isMultiple(of: 2)}.map{"\($0) is a lucky number \n"}.joined())
+
+// Day 11
+// access control, static properties and methods, and checkpoint 6
 
 struct Car {
     let model: String
@@ -21,6 +27,9 @@ struct Car {
 var car = Car(model: "Alfa Romeo 147", seats: 5)
 car.changeGear(difference: 1)
 print(car.gear)
+
+// Day 12
+// classes, inheritance, and checkpoint 7
 
 class Animal {
     let legs: Int
@@ -71,4 +80,80 @@ class Lion: Cat {
 }
 
 // Day 13
+// Protocols, extensions and checkpoint 8
 
+// Sample extension
+
+extension Int {
+    func cubed() -> Int {
+        return self * self * self
+    }
+}
+
+// Protocol extensions
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
+
+// Checkpoint 8
+
+protocol Building {
+    var type: String { get }
+    var rooms: Int { get }
+    var cost: Int { get set }
+    var agent: String { get set }
+    func printSummary()
+}
+
+extension Building {
+    func printSummary() {
+        print("Talk to \(agent) to buy this \(type) for $\(cost)")
+    }
+}
+
+struct House: Building {
+    let type = "house"
+    var rooms: Int
+    var cost: Int
+    var agent: String
+}
+
+struct Office: Building {
+    let type = "office"
+    var rooms: Int
+    var cost: Int
+    var agent: String
+}
+
+let exampleHouse = House(rooms: 4, cost: 500_000, agent: "Swift & Co")
+exampleHouse.printSummary()
+
+let exampleOffice = Office(rooms: 30, cost: 12000000, agent: "Taytay inc")
+exampleOffice.printSummary()
