202eb5bee90bb994aa2560d8d5a544242686f959
Miks Latvis <miks.latvis@gmail.com>
2022-01-31 20:00:32 +0900

    Day 29 begins

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Miks Latvis on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
