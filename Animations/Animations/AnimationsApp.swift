4c592516900deb2b8e8201326a1fe86fdbf335d0
Miks Latvis <miks.latvis@gmail.com>
2022-02-02 22:26:05 +0900

    Start of day 32

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Miks Latvis on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
