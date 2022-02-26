9772aff6723e3b2cc3a77418a7394a88ed53e335
Miks Latvis <miks.latvis@gmail.com>
2022-02-26 14:20:25 +0900

    End of day 40

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Miks Latvis on 21/02/2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
