d241825cf1873ffeca2461cc78822a1178b845d4
Miks Latvis <miks.latvis@gmail.com>
2022-03-01 22:08:39 +0900

    Before day 44

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Miks Latvis on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
