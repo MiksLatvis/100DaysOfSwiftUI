82e7e5ee43932a849f63a561cda9bea6abde2139
Miks Latvis <miks.latvis@gmail.com>
2022-03-03 21:51:13 +0900

    End of day 45

---
+++ Spirograph/Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Miks Latvis on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
